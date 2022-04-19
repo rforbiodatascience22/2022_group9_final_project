c061b1609ad9b87183569f6d37d295d93c434eb8
Yi9898 <yihuang9898@gmail.com>
2022-04-19 09:00:49 +0000

    added notes

--- R/00_doit.R
+++ R/00_doit.R
@@ -3,3 +3,5 @@ source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
 source(file = "R/04_analysis_i.R")
+
+# Make sure to include a programmatic call to knitr at the end of your doit-script
