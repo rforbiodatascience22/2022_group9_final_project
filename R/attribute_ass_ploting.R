2fdcc9ae0ae6480140c68427670b07ac7a778897
Yu950831 <s212610@dtu.dk>
2022-04-25 18:22:57 +0000

    annotation

--- R/attribute_ass_ploting.R
+++ R/attribute_ass_ploting.R
@@ -1,4 +1,8 @@
 # Load libraries ----------------------------------------------------------
 library("ggplot2")
 
-##
+## as we would like to selcet some of the attributes.
+## we choose to visualize all the values in the BC_data attributes based on the
+## subtypes of the cancer
+
+## plot of 
