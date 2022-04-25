545acae0d359b9f6cab6ecfd5650794f5fdeecf1
Yu950831 <s212610@dtu.dk>
2022-04-25 21:07:42 +0000

    remove

--- R/attribute_ass_ploting.R
+++
@@ -1,8 +0,0 @@
-# Load libraries ----------------------------------------------------------
-library("ggplot2")
-
-## as we would like to selcet some of the attributes.
-## we choose to visualize all the values in the BC_data attributes based on the
-## subtypes of the cancer
-
-## plot of 
