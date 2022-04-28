24ab9075509b949111b2d310bbfb38b54f5316bc
AnneRossen <anne@hjaltesvej.dk>
2022-04-28 13:15:23 +0000

    merged wrong

--- R/04_analysis_glm.R
+++ R/04_analysis_glm.R
@@ -18,18 +18,22 @@ BC_data_clean_aug <- read_csv(file = "data/03_BC_data_clean_aug.csv")
 
 # Wrangle data ------------------------------------------------------------
 # Add subtype to Gene Expression data
-<<<<<<< HEAD
+
+
+#### WHICH ONE OF THESE ?
 Expresion_Subtype <- left_join(proteomes_clean_aug,
-                                    BC_data_clean_aug,
-                                    by = c("TCGA ID" = "Complete TCGA ID")) %>%
+                               BC_data_clean_aug,
+                               by = c("TCGA ID" = "Complete TCGA ID")) %>%
   column_to_rownames(var = "TCGA ID") %>% 
   select(c(1:12553), "Subtype")
-=======
+
+
 Expresion_Subtype <- BC_data_clean_aug %>%
   column_to_rownames(var = "Complete TCGA ID") %>% 
   select(-c(1:29)) %>% 
   relocate(Subtype)
->>>>>>> 9f24866f1c91c060df444e040a654955095a0794
+#### WHICH ONE OF THESE ?
+
 
 
 # Create long table
@@ -45,16 +49,14 @@ Expresion_Subtype_long_nested <- Expresion_Subtype_long %>%
   ungroup()
 
 
-<<<<<<< HEAD
 # Model data
 #my_data_clean_aug %>% ...
-=======
+
 # Model data ---------------------------------------------------------------
 # Build glm model
 Expresion_Subtype_long_nested <- Expresion_Subtype_long_nested %>% 
   mutate(mdl = map(data, ~glm(Subtype ~ log2_expr_level,
                               data = .x)))
->>>>>>> 9f24866f1c91c060df444e040a654955095a0794
 
 # Clean up model objects
 Expresion_Subtype_long_nested <- Expresion_Subtype_long_nested %>% 
@@ -64,11 +66,8 @@ Expresion_Subtype_long_nested <- Expresion_Subtype_long_nested %>%
   mutate(identified_as = case_when(p.value < 0.05 ~ "significant",    # Add an indicator variable
                                    p.value >= 0.05 ~ "insignificant")) 
 
-<<<<<<< HEAD
 # Visualise data ----------------------------------------------------------
 #my_data_clean_aug %>% ...
-=======
->>>>>>> 9f24866f1c91c060df444e040a654955095a0794
 
 # Visualise data -----------------------------------------------------------
 Expresion_Subtype_long_nested %>% 
@@ -89,12 +88,7 @@ Expresion_Subtype_long_nested %>%
   labs(y = "") 
 ### We need to think of another way of visualization
 
-<<<<<<< HEAD
-# Write data --------------------------------------------------------------
-#write_tsv(...)
-#ggsave(...)
-=======
+
 # Write data ---------------------------------------------------------------
 write_csv(Expresion_Subtype_long_nested, file = "data/04_glm_result.csv")
 ggsave(...)
->>>>>>> 9f24866f1c91c060df444e040a654955095a0794
