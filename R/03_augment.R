a241d6d0fcc728722eb5c9836829ee29cf68e8d1
synnespjelkavik <s174526@student.dtu.dk>
2022-04-28 14:23:18 +0000

    Adding logical columns to BC_data_clean_aug for subtypes

--- R/03_augment.R
+++ R/03_augment.R
@@ -23,10 +23,11 @@ BC_data_clean_aug <- BC_data_clean_aug %>%
   mutate(Subtype = case_when(`PAM50 mRNA` == "Luminal A" ~ 0,
                              `PAM50 mRNA` == "Luminal B" ~ 1,
                              `PAM50 mRNA` == "HER2-enriched" ~ 2,
-                             `PAM50 mRNA` == "Basal-like" ~ 3))
-
-BC_data_clean_aug$Subtype
-
+                             `PAM50 mRNA` == "Basal-like" ~ 3),
+         Luminal_A = if_else(`PAM50 mRNA` == "Luminal A", 1, 0),
+         Luminal_B = if_else(`PAM50 mRNA` == "Luminal B", 1, 0),
+         HER2_enriched = if_else(`PAM50 mRNA` == "HER2-enriched", 1, 0),
+         Basal_like = if_else(`PAM50 mRNA` == "Basal-like", 1, 0))
 
 # Write data --------------------------------------------------------------
 write_csv(x = patients_clean_aug,
