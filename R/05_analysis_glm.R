7f15f7776d11302ab192c3450574ce27502ecebb
Yi9898 <yihuang9898@gmail.com>
2022-05-02 09:02:32 +0000

    built subtype_glm() and structed the glm script

--- R/05_analysis_glm.R
+++ R/05_analysis_glm.R
@@ -6,53 +6,36 @@ library("purrr")
 library("vroom")
 
 
-# Define functions --------------------------------------------------------
+# Define functions -------------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
 
-# Load data ---------------------------------------------------------------
+# Load data --------------------------------------------------------------------
 proteomes_clean_aug <- read_csv(file = "data/03_proteomes_clean_aug.csv")
 BC_data_clean_aug   <- read_csv(file = "data/03_BC_data_clean_aug.csv")
 
 
-# Wrangle data ------------------------------------------------------------
 
-# Wrangling data so that it can be used for glm
-BC_data_clean_aug_test <- BC_data_clean_aug %>%
-  select(c(Luminal_A,
-           Luminal_B,
-           HER2_enriched,
-           Basal_like,
-           starts_with("NP"))) %>% 
-  pivot_longer(cols = -c(Luminal_A,
-                         Luminal_B,
-                         HER2_enriched,
-                         Basal_like),
-               names_to = "proteom",
-               values_to = "expr_level") %>% 
-  group_by(proteom) %>% 
-  nest() %>% 
-  ungroup()
-
+# Model data -------------------------------------------------------------------
 # Making four different logistic models for each of the subtypes
+# Luminal A
+LumA_glm <- subtype_glm("Luminal_A", BC_data_clean_aug)
 
-# Luminal A --------------------------------------------------------------------
-BC_data_clean_aug_LuminalA = BC_data_clean_aug_test %>%
-  mutate(mdl_LuminalA = map(data, ~glm(Luminal_A ~ expr_level,
-                                       data = .,
-                                       family = binomial(link = "logit"))),
-         mdl_LuminalA_tidy = map(mdl_LuminalA, tidy, conf.int = TRUE)) %>% 
-  unnest(mdl_LuminalA_tidy) %>% 
-  filter(term != "(Intercept)") %>% 
-  mutate(identified_as_LuminalA = case_when(p.value < 0.05 ~ "significant",    # Add an indicator variable
-                                   p.value >= 0.05 ~ "insignificant"),
-         identified_as_LuminalA = as.factor(identified_as_LuminalA))
-  
+# Luminal B
+LumB_glm <- subtype_glm("Luminal_B", BC_data_clean_aug)
 
+# HER2_enriched
+Her2_glm <- subtype_glm("HER2_enriched", BC_data_clean_aug)
+
+# Basal-like
+Basal_glm <- subtype_glm("Basal_like", BC_data_clean_aug)
+
+
+# Visualise data ---------------------------------------------------------------
 # Density plot of Luminal A
-BC_data_clean_aug_LuminalA %>%
+LumA_glm %>%
   ggplot(aes(estimate,
-             fill = identified_as_LuminalA)) +
+             fill = identified_as)) +
   geom_density(alpha = 0.6) +
   theme_classic(base_family = "Avenir",
                 base_size = 8) +
@@ -63,23 +46,11 @@ BC_data_clean_aug_LuminalA %>%
 #BC_data_clean_aug_LuminalA %>% filter(identified_as_LuminalA == "significant")
 # Reduction: 9274 to 1,861
 
-# Luminal B --------------------------------------------------------------------
-
-BC_data_clean_aug_LuminalB = BC_data_clean_aug_test %>%
-  mutate(mdl_LuminalB = map(data, ~glm(Luminal_B ~ expr_level,
-                                       data = .,
-                                       family = binomial(link = "logit"))),
-         mdl_LuminalB_tidy = map(mdl_LuminalB, tidy, conf.int = TRUE)) %>% 
-  unnest(mdl_LuminalB_tidy) %>% 
-  filter(term != "(Intercept)") %>% 
-  mutate(identified_as_LuminalB = case_when(p.value < 0.05 ~ "significant",    # Add an indicator variable
-                                            p.value >= 0.05 ~ "insignificant"),
-         identified_as_LuminalB = as.factor(identified_as_LuminalB))
 
 # Density plot of Luminal B
-BC_data_clean_aug_LuminalB %>%
+LumB_glm %>%
   ggplot(aes(estimate,
-             fill = identified_as_LuminalB)) +
+             fill = identified_as)) +
   geom_density(alpha = 0.6) +
   theme_classic(base_family = "Avenir",
                 base_size = 8) +
@@ -90,23 +61,10 @@ BC_data_clean_aug_LuminalB %>%
 # 9,274 to 1,435
 
 
-# HER2 enriched --------------------------------------------------------------------
-
-BC_data_clean_aug_HER2 = BC_data_clean_aug_test %>%
-  mutate(mdl_HER2 = map(data, ~glm(HER2_enriched ~ expr_level,
-                                       data = .,
-                                       family = binomial(link = "logit"))),
-         mdl_HER2_tidy = map(mdl_HER2, tidy, conf.int = TRUE)) %>% 
-  unnest(mdl_HER2_tidy) %>% 
-  filter(term != "(Intercept)") %>% 
-  mutate(identified_as_HER2 = case_when(p.value < 0.05 ~ "significant",    # Add an indicator variable
-                                            p.value >= 0.05 ~ "insignificant"),
-         identified_as_HER2 = as.factor(identified_as_HER2))
-
-# Plot
-BC_data_clean_aug_HER2 %>%
+# Density plot of HER2-enriched
+Her2_glm %>%
   ggplot(aes(estimate,
-             fill = identified_as_HER2)) +
+             fill = identified_as)) +
   geom_density(alpha = 0.6) +
   theme_classic(base_family = "Avenir",
                 base_size = 8) +
@@ -114,104 +72,20 @@ BC_data_clean_aug_HER2 %>%
         legend.position = "bottom")
 
 
-# Basal-like -------------------------------------------------------------------
-
-BC_data_clean_aug_basal = BC_data_clean_aug_test %>%
-  mutate(mdl_basal = map(data, ~glm(Basal_like ~ expr_level,
-                                       data = .,
-                                       family = binomial(link = "logit"))),
-         mdl_basal_tidy = map(mdl_basal, tidy, conf.int = TRUE)) %>% 
-  unnest(mdl_basal_tidy) %>% 
-  filter(term != "(Intercept)") %>% 
-  mutate(identified_as_basal = case_when(p.value < 0.05 ~ "significant",    # Add an indicator variable
-                                            p.value >= 0.05 ~ "insignificant"),
-         identified_as_basal = as.factor(identified_as_basal))
-
-# Density plot
-BC_data_clean_aug_basal %>%
+# Density plot of Basal-like
+Basal_glm %>%
   ggplot(aes(estimate,
-             fill = identified_as_basal)) +
+             fill = identified_as)) +
   geom_density(alpha = 0.6) +
   theme_classic(base_family = "Avenir",
                 base_size = 8) +
   theme(axis.text.y = element_text(),
         legend.position = "bottom")
-
-
 # 9,274 to 2,503
 
 
-# TRIED TO PUT EVERYTHING IN ONE, BUT THINK IT IS TOO LONG
-#BC_data_clean_aug_test_model = BC_data_clean_aug_test %>%
-#  mutate(mdl_LuminalA = map(data, ~glm(Luminal_A ~ expr_level,
-#                                       data = .x,
-#                                       family = binomial(link = "logit"))),
-#         mdl_LuminalB = map(data, ~glm(Luminal_B ~ expr_level,
-#                                       data = .x,
-#                                       family = binomial(link = "logit"))),
-#         mdl_HER2 = map(data, ~glm(HER2_enriched ~ expr_level,
-#                                   data = .x,
-#                                   family = binomial(link = "logit"))),
-#         mdl_Basal = map(data, ~glm(Basal_like ~ expr_level,
-#                                    data = .x,
-#                                    family = binomial(link = "logit"))))
 
-# ------------------------------------------------------------------------------
-
-gene_expr_data_long_nested = gene_expr_data_long_nested %>%
-  mutate(mdl_tidy = map(mdl, ~tidy(.x, conf.int = TRUE))) %>% 
-  unnest(mdl_tidy)
-gene_expr_data_long_nested
-
-#BC_data_clean_aug_test_model = BC_data_clean_aug_test %>%
-#  mutate(mdl = map(data, ~glm(response ~ expr_lvl,
-#                              data = subset(BC_data_clean_aug_test == 0),
-#                              family = binomial(link = "logit"))))
-
-
-# Add subtype to Gene Expression data
-Expresion_Subtype <- BC_data_clean_aug %>%
-  column_to_rownames(var = "Complete TCGA ID") %>% 
-  select(-c(1:29)) %>% 
-  relocate(Subtype)
-
-
-
-
-# Create long table
-Expresion_Subtype_long <- Expresion_Subtype %>% 
-  pivot_longer(cols = -Subtype,
-               names_to = "protein",
-               values_to = "log2_expr_level")
-
-# Create nested tibble
-Expresion_Subtype_long_nested <- Expresion_Subtype_long %>% 
-  group_by(protein) %>% 
-  nest() %>% 
-  ungroup()
-
-
-# Model data
-#my_data_clean_aug %>% ...
-
-# Model data ---------------------------------------------------------------
-# Build glm model
-Expresion_Subtype_long_nested <- Expresion_Subtype_long_nested %>% 
-  mutate(mdl = map(data, ~glm(Subtype ~ log2_expr_level,
-                              data = .x)))
-
-# Clean up model objects
-Expresion_Subtype_long_nested <- Expresion_Subtype_long_nested %>% 
-  mutate(tidy = map(mdl, broom::tidy, conf.int = TRUE)) %>%
-  unnest(tidy) %>% 
-  filter(term != "(Intercept)") %>%  # Remove the (Intercept)-rows
-  mutate(identified_as = case_when(p.value < 0.05 ~ "significant",    # Add an indicator variable
-                                   p.value >= 0.05 ~ "insignificant")) 
-
-# Visualise data ----------------------------------------------------------
-#my_data_clean_aug %>% ...
-
-# Visualise data -----------------------------------------------------------
+# Visualize the model
 Expresion_Subtype_long_nested %>% 
   filter(identified_as == "significant") %>% 
   ggplot(aes(x = estimate,
@@ -232,19 +106,16 @@ Expresion_Subtype_long_nested %>%
 
 
 # Write data ---------------------------------------------------------------
-write_csv(Expresion_Subtype_long_nested, 
-          file = "data/04_glm_result.csv")
+write_csv(LumA_glm, 
+          file = "results/05_LumA_glm.csv")
 
-write_csv(BC_data_clean_aug_LuminalA, 
-          file = "data/04_glm_result.csv")
+write_csv(LumB_glm, 
+          file = "results/05_LumB_glm.csv")
 
-write_csv(BC_data_clean_aug_LuminalB, 
-          file = "data/04_glm_result.csv")
+write_csv(Her2_glm, 
+          file = "results/05_Her2_glm.csv")
 
-write_csv(BC_data_clean_aug_HER2, 
-          file = "data/04_glm_result.csv")
-
-write_csv(BC_data_clean_aug_basal, 
-          file = "results/04_glm_result.csv")
+write_csv(Basal_glm, 
+          file = "results/05_Basal_glm.csv")
 
 ggsave(...)
--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -8,3 +8,24 @@ change_format <- function(ID){
 
 
 count_na_func <- function(x) sum(is.na(x))
+
+subtype_glm <- function(subtype, BC_data){
+  result <- BC_data %>% 
+    select(subtype, starts_with("NP")) %>% 
+    pivot_longer(cols = -subtype,
+                 names_to = "proteome",
+                 values_to = "expr_level") %>% 
+    group_by(proteome) %>% 
+    nest() %>% 
+    ungroup() %>% 
+    mutate(mdl = map(data, ~glm(as.formula(paste(subtype, " ~ expr_level", sep = "")),
+                                data = .,
+                                family = binomial(link = "logit"))),
+           mdl_tidy = map(mdl, tidy, conf.int = TRUE)) %>% 
+    unnest(mdl_tidy) %>% 
+    filter(term != "(Intercept)") %>% 
+    mutate(identified_as = case_when(p.value < 0.05 ~ "significant",    # Add an indicator variable
+                                     p.value >= 0.05 ~ "insignificant"),
+           identified_as = as.factor(identified_as))
+  return(result)
+}
