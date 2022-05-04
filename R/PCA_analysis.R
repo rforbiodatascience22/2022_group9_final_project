8f67caf016dedb4864f8f0bb260368d85b4cccf1
Yu950831 <s212610@dtu.dk>
2022-05-04 21:48:38 +0000

    common genes clustering predict

--- R/PCA_analysis.R
+++ R/PCA_analysis.R
@@ -8,64 +8,72 @@ library(cowplot)
 library(patchwork) # required to arrange plots side-by-side
 library(ggthemes) # for colorblind color scale
 library(dplyr)
+library(vroom)
+library(purrr)
+rm(list = ls())
 
-BC_PCA <- BC_data_clean %>% 
-  select(29:33) %>% # as there are too much demensions I decide to have 
-  # the first 4 dimensions 
-  scale() %>% 
-  prcomp() ## run PCA
+### load the data 
+BC_overlap_genes <- read.csv('results/06_BC_overlap_genes.csv')
+BC_overlap_genes
 
-view(BC_PCA)
-view(head(BC_PCA$x)) ## check the standard division contributions
+### Principal Conponent Analysis
+BC_overlap_genes_pca <- BC_overlap_genes %>% select(-c(PAM50.mRNA)) %>% 
+prcomp(center = TRUE, scale. = T)
 
-### add the subtypes to the PCA data and visualize it.
-BC_PCA_plt <- data.frame(BC_PCA$x,
-                         subtype = BC_data_clean$`PAM50 mRNA`)
-head(BC_PCA_plt)
+### 
+BC_overlap_genes_pca %>% tidy('pcs') %>% 
+  ggplot(aes(x = PC,
+             y = percent)) + geom_col() +
+  theme_bw()
 
-### visualization
-ggplot(BC_PCA_plt,aes(x = PC1,
-                      y = PC2,
-                      col = subtype)) + geom_point() +  
-  scale_color_colorblind()
+BC_pca_plt <- data.frame(BC_overlap_genes_pca$x,subtype = BC_overlap_genes$PAM50.mRNA)
 
-### view the rotation matrix
-View(BC_PCA$rotation)
+### augment the data  
+BC_aug <- BC_overlap_genes_pca %>% augment(BC_overlap_genes)
+BC_aug
 
-### rotation visualize 
-rotation_data <- BC_PCA$rotation
+### the bar plot
+BC_aug %>% ggplot(aes(x= .fittedPC1, 
+                      y = .fittedPC2,
+                      col = PAM50.mRNA)) + geom_point() +
+scale_color_colorblind()
 
-### reduce  the arrorws 
-rotation_data <- data.frame(BC_PCA$rotation,
-                            variable = row.names(BC_PCA$rotation))
+### cluster 
+BC_K_org <- BC_aug %>% 
+  select(contains('NP')) %>%
+  kmeans(centers = 4)
+BC_K_org
 
-# define a pleasing arrow style
-arrow_style <- arrow(
-  length = unit(0.05, "inches"),
-  type = "closed")
+BC_aug_K_org <- BC_K_org %>% augment(BC_aug) %>% 
+  rename(cluster_org = .cluster)
+BC_aug_K_org
 
-# now plot, using geom_segment() for arrows and geom_text() for labels
-ggplot(rotation_data) + 
-  geom_segment(aes(xend = PC1, 
-                   yend = PC2), 
-               x = 0,
-               y = 0, 
-               arrow = arrow_style) + 
-  geom_text(aes(x = PC1, 
-                y = PC2, 
-                label = variable), 
-            hjust = 0, 
-            size = 3, 
-            color = "red") + 
-  xlim(-1., 1.25) + 
-  ylim(-1., 1.) +
-  coord_fixed() # fix aspect ratio to 1:1
-
-percent <- 100*BC_PCA$sdev^2 / sum(BC_PCA$sdev^2)
-percent
-perc_data <- data.frame(percent = percent, PC = 1:5)
-ggplot(perc_data, aes(x = PC, y = percent)) + geom_col()
+### compare the Kmean cluster to the grandtruth 
+pl1 = ggplot(BC_aug_K_org,aes(x=.fittedPC1,
+                              y=.fittedPC2,
+                              col = PAM50.mRNA)) + geom_point() +
+  theme(legend.position = "bottom")
 
 
-### as we can see that the demension1 contains over 99% of the information 
-### I suggest that we make a only one dimension plot 
+pl2 = ggplot(BC_aug_K_org,aes(x=.fittedPC1,
+                              y=.fittedPC2,
+                              col =cluster_org)) + geom_point() +
+  theme(legend.position = "bottom")
+
+pl1 + pl2
+
+### calculate the accuracy of the prediction
+BC_aug_K_org %>% 
+  select(PAM50.mRNA,cluster_org) %>% 
+  mutate(predict = case_when(cluster_org == 1 ~ 'HER2-enriched',
+                             cluster_org == 2 ~ 'Luminal B',
+                             cluster_org == 3 ~ 'Basal-like',
+                             cluster_org == 4 ~ 'Luminal A'),
+         cluster_org_correct = case_when(predict == PAM50.mRNA ~ 1,
+                                         predict != PAM50.mRNA ~ 0)) %>% 
+  summarise(score_org = mean(cluster_org_correct))
+
+### from the result we can see that we have only 0.418 accuracy. 
+### seems not so good.
+
+
