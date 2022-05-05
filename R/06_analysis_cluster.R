72c3a0fac3bb3bc8d583cdcf4d1acec0663b41f8
synnespjelkavik <s174526@student.dtu.dk>
2022-05-05 09:17:36 +0000

    PCA and cluster analysis on the original BC_data and the one where we only look into the ones that are common for both the BC_data and the PAM50 data set. Scatter plots and plots of the percentage of variance explained by each PCA

--- R/06_analysis_cluster.R
+++ R/06_analysis_cluster.R
@@ -12,33 +12,166 @@ source(file = "R/99_project_functions.R")
 BC_data_clean_aug   <- read_csv(file = "data/03_BC_data_clean_aug.csv")
 
 
-pca_fit <- BC_data_clean_aug %>% 
+# Modifying data files ---------------------------------------------------------
+# Transforming PAM50 data
+PAM50_selected <- PAM50 %>%
+  pivot_longer(cols = -RefSeqProteinID) %>%
+  pivot_wider(names_from = RefSeqProteinID) %>%
+  rename("RefSeqProteinID" = 1) %>%
+  as_tibble()
+
+# Making a list containing the column names
+PAM50_selected_names <- PAM50_selected %>% 
+  names() %>% 
+  select(-c(1))
+
+BC_data_clean_aug_names <- BC_data_clean_aug %>% 
+  names()
+
+# Selecing the ones that are the same for both
+BC_data_clean_aug_PAM50 <- BC_data_clean_aug %>% 
+  select(c(1:28),
+         BC_data_clean_aug_names[BC_data_clean_aug_names %in% PAM50_selected_names])
+
+
+# PCA analysis  ----------------------------------------------------------------
+
+# Doing a PCA analysis - only numerical data for protein IDs
+pca_fit_ori <- BC_data_clean_aug %>% 
   select(c(29:8022)) %>% 
   select(where(is.numeric)) %>% # retain only numeric columns
-  prcomp(scale = TRUE)
+  prcomp(center = TRUE, scale. = TRUE)
 
+# Adding original data back
+pca_fit_ori_aug <- pca_fit_ori %>% 
+  augment(BC_data_clean_aug)
 
+# PCA on reduced version
+pca_fit_reduced <- BC_data_clean_aug_PAM50 %>% 
+  select(c(29:54)) %>% 
+  select(where(is.numeric)) %>% # retain only numeric columns
+  prcomp(center = TRUE, scale. = TRUE)
 
-pca_fit %>%
-  augment(BC_data_clean_aug) %>% # add original dataset back in
-  ggplot(aes(.fittedPC1, .fittedPC2, color = `PAM50 mRNA`)) + 
+# Adding original data back
+pca_fit_red_aug <- pca_fit_reduced %>% 
+  augment(BC_data_clean_aug_PAM50)
+
+# Visualizing the results  -----------------------------------------------------
+
+# Scatter plot of 1st and 2nd PC - Original data
+pca_fit_original_plot <- pca_fit_ori_aug %>% # add original dataset back in
+  ggplot(aes(.fittedPC1, 
+             .fittedPC2, 
+             color = `PAM50 mRNA`)) + 
   geom_point(size = 1.5) +
-  theme_half_open(12) + background_grid()
+  theme_half_open(12) + 
+  background_grid()
 
+# Scatter plot of 1st and 2nd PC - Reduced data
+pca_fit_reduced_plot <- pca_fit_red_aug %>%
+  ggplot(aes(.fittedPC1, 
+             .fittedPC2, 
+             color = `PAM50 mRNA`)) + 
+  geom_point(size = 1.5) +
+  theme_half_open(12) + 
+  background_grid()
 
-pca_fit %>%
-  tidy(matrix = "eigenvalues") %>%
+# Variance explained by each PC (plots) - Original data
+pca_fit_original_PC_plot <- pca_fit_original %>%
+  tidy("pcs") %>%
   ggplot(aes(PC, percent)) +
   geom_col(fill = "#56B4E9", alpha = 0.8) +
-  scale_x_continuous(breaks = 1:9) +
   scale_y_continuous(
     labels = scales::percent_format(),
-    expand = expansion(mult = c(0, 0.01))
-  ) +
-  theme_minimal_hgrid(12)
+    expand = expansion(mult = c(0, 0.01))) +
+  theme_half_open(12)
 
 
+# Variance explained by each PC (plots) - Reduced data
+pca_fit_reduced_PC_plot <- pca_fit_reduced %>%
+  tidy("pcs") %>%
+  ggplot(aes(PC, percent)) +
+  geom_col(fill = "#56B4E9", alpha = 0.8) +
+  scale_y_continuous(
+    labels = scales::percent_format(),
+    expand = expansion(mult = c(0, 0.01))) +
+  theme_half_open(12)
 
+# Plots all together
+(pca_fit_original_plot + pca_fit_reduced_plot +  plot_layout(guides = 'auto'))/
+  (pca_fit_original_PC_plot + pca_fit_reduced_PC_plot) + 
+  plot_layout(guides = 'collect') & 
+  theme(legend.position = "top")
+
+
+# K-means analysis (original data) ---------------------------------------------
+set.seed(4)
+
+my_k_org <- pca_fit_ori_aug %>%
+  select(.fittedPC1, .fittedPC2) %>%
+  kmeans(centers = 4)
+
+my_pca_aug_k_org <- my_k_org %>%
+  augment(pca_fit_ori_aug) %>% 
+  rename(cluster_org = .cluster)
+my_pca_aug_k_org
+
+
+pl1 <- my_pca_aug_k_org %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = `PAM50 mRNA`)) +
+  geom_point() +
+  theme(legend.position = "bottom")
+
+pl2 <- my_pca_aug_k_org %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = cluster_org)) +
+  geom_point() +
+  theme(legend.position = "bottom")
+
+pl1 + pl2
+
+
+# K-means analysis (Reduced data) ----------------------------------------------
+set.seed(4)
+my_k_red <- pca_fit_red_aug %>%
+  select(.fittedPC1, .fittedPC2) %>%
+  kmeans(centers = 4)
+
+my_pca_aug_k_red <- my_k_red %>%
+  augment(pca_fit_red_aug) %>% 
+  rename(cluster_org = .cluster)
+my_pca_aug_k_red
+
+
+pl3 <- my_pca_aug_k_red %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = `PAM50 mRNA`)) +
+  geom_point() +
+  theme(legend.position = "bottom")
+
+pl4 <- my_pca_aug_k_red %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = cluster_org)) +
+  geom_point() +
+  theme(legend.position = "bottom")
+
+pl3 + pl4
+
+
+# TRASH ?? ---------------------------------------------------------------------
+
+my_k_red <- pca_fit_reduced %>%
+  augment(BC_data_clean_aug_PAM50) %>% 
+  select(.fittedPC1, .fittedPC2) %>%
+  kmeans(centers = 4)
+
+
+pl1 <- my_k_org %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = `PAM50 mRNA`)) +
+  geom_point() +
+  theme(legend.position = "bottom")
+
+pl2 <- my_pca_aug_k_org_pca %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = cluster_org)) +
+  geom_point() +
+  theme(legend.position = "bottom")
 
 
 
