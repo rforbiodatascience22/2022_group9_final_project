0d4e0344b9fadf67de3901aad7fefe7e9fe546b8
Yi9898 <yihuang9898@gmail.com>
2022-05-08 15:32:19 +0000

    Added source

--- R/00_doit.R
+++ R/00_doit.R
@@ -1,10 +1,12 @@
 # Run all scripts ---------------------------------------------------------
+source(file = "R/00_doit.R")
 source(file = "R/01_load.R")
-source(file = "R/01_PlotData.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
+source(file = "R/04_PlotData.R")
 source(file = "R/05_analysis_glm.R")
 source(file = "R/06_analysis_PickGenes.R")
+source(file = "R/07_analysis_PAM50_clustering.R")
 
-# Add all files
-# Make sure to include a programmatic call to knitr at the end of your doit-script
+# knit
+
